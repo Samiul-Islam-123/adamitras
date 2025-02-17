304e56d6e9907104cf53a33e1d6cdc9ff5fc8c0f
Sourish Samanta <sourishsamanta1234@gmail.com>
2025-02-17 23:50:51 +0530

    frontend

--- client/src/components/Pyq.jsx
+++ client/src/components/Pyq.jsx
@@ -21,6 +21,9 @@ const Pyq = () => {
         <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/3 md:w-[40%] w-[85%] border-[#EFC740] border-2 shadow-md shadow-black/20 flex items-center justify-center flex-col gap-5 rounded-3xl  bg-[#FFF4CE] py-20 '>
           <h1 className=' text-3xl mb-5 font-bold text-[#f5c72f]'>Select Your Need</h1>
           <select className=' w-[200px] border-[#EFC740] border-2 py-2 px-4 rounded-xl'  name="" id="">
+          <option value="" disabled selected>Select Course</option>
+          </select>
+          <select className=' w-[200px] border-[#EFC740] border-2 py-2 px-4 rounded-xl'  name="" id="">
           <option value="" disabled selected>Select Semester</option>
           </select>
           <select className=' w-[200px] py-2 border-[#EFC740] border-2 px-4 rounded-xl'  name="" id="">
--- client/src/components/ShowPyqs.jsx
+++ client/src/components/ShowPyqs.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 
 const ShowPyqs = ({handleclick}) => {
     return (
         <div className="absolute top-0 left-0 z-20 w-screen h-screen bg-black/15 flex pt-32 justify-center">
             <div className=" flex w-full  h-[70%]  justify-center">
-                <div className=" w-[60%] bg-[#FFF4CE] border-4 border-[#EFC740] rounded-3xl">
-                  <div className=" flex gap-3 p-20">
-                    <input type="text" className=" focus:outline-none py-2 px-3 w-60 border-2 border-[#EFC740] rounded-md"/>
-                    <button className=" p-3 bg-[#EFC740] rounded-md text-white">Search</button>
+                <div className=" md:w-[60%] w-[80%]  bg-[#FFF4CE] border-4 border-[#EFC740] rounded-3xl">
+                  <div className=" flex gap-3 md:p-20 py-6 px-2">
+                    <input type="text" className=" focus:outline-none py-2 md:px-3 px-2 w-60 border-2 border-[#EFC740] rounded-md"/>
+                    <button className=" p-3  bg-[#EFC740] rounded-full text-white"><FaSearch /></button>
                   </div>
                   <div className=" w-full flex-1">
                     <ul>
-                      <li className=" mx-16 py-3 cursor-pointer flex justify-between border-b border-black/30 text-lg ">
-                        <h1 className=" font-semibold">Engineering Mathematic-I</h1>
-                        <p>2016</p>
-                        <button className=" px-3 py-1 bg-[#EFC740] rounded-md text-white">See</button>
+                      <li className=" md:mx-16 mx-3 py-3 cursor-pointer flex justify-between border-b border-black/30 text-lg ">
+                        <h1 className=" font-semibold md:text-lg text-sm">Engineering Mathematic-I</h1>
+                        <p className="md:text-lg text-sm">2016</p>
+                        <button className=" md:px-3 md:py-1 px-10 md:text-lg text-sm bg-[#EFC740] rounded-md text-white md:block hidden ">See</button>
                       </li>
                     </ul>
                   </div>
