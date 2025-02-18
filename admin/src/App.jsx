963332dce17f83ae4c2c17d470b8c64e102080ef
MD Samiul Islam <isamiul099@gmail.com>
2025-02-19 00:47:06 +0530

    created editor

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,27 +1,15 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import Panel from './component/Panel'
-import Login from './component/Login'
-import Signup from './component/Signup'
-import { useUser } from '@clerk/clerk-react'
-import './index.css'
-const ProtectedRoute = ({ children }) => {
-  const { isSignedIn } = useUser();
-
-
-  return isSignedIn ? children : <Navigate to="/login" />;
-};
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BlogEditor from './components/editor'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Routes>
-
-        <Route path="/" element={<Panel />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-
-      </Routes>
+      <BlogEditor />
     </>
   )
 }
