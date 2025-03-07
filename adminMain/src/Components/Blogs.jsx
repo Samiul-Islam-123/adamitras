d8e2c7da3577603b9b16f8a763997e419287fa40
samiul <isamiul099@gmail.com>
2025-03-07 14:57:49 +0530

    something is done

--- adminMain/src/Components/Blogs.jsx
+++ adminMain/src/Components/Blogs.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TextEditor from './TextEditor'
+import axios from 'axios'
 
 const Blogs = () => {
+
+  const fetchBlogs = async() => {
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/blog/blogs`);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  },[])
+
   return (
     <div>
         
